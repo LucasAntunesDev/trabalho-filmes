7dc1764d3be73e6fe4d2967877f37c33a23dd179
Lucas Antunes <lucasantuneslara06@gmail.com>
2024-09-10 08:52:58 -0300

    add: Usuário não admin na seeder

--- database/seeders/UsuariosSeeder.php
+++ database/seeders/UsuariosSeeder.php
@@ -12,11 +12,21 @@ class UsuariosSeeder extends Seeder {
      * Run the database seeds.
      */
     public function run(): void {
-        DB::table('usuarios')->insert([
-            'name' => 'admin',
-            'username' => 'admin',
-            'password' => Hash::make('admin'),
-            'admin' => 1,
-        ],);
+        DB::table('usuarios')->insert(
+            [
+                [
+                    'name' => 'admin',
+                    'username' => 'admin',
+                    'password' => Hash::make('admin'),
+                    'admin' => 1,
+                ],
+                [
+                    'name' => 'user',
+                    'username' => 'user',
+                    'password' => Hash::make('user'),
+                    'admin' => 0,
+                ]
+            ],
+        );
     }
 }
